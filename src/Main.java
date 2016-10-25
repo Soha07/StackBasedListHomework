65a02fe849b99dcea564ce1af12bd5dc6bd6dd62
sardonyx072 <mithoffman11@gmail.com>
2016-10-24 22:37:19 -0500

    updated for side by side of Tight and Growth and more data points

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,38 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main (String[] args) {
+		String integers = "";
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\hoff3539\\workspace\\StackBasedListHomework\\res\\data.csv"));
+			BufferedReader br = new BufferedReader(new FileReader("res\\data.csv"));
 			StringBuilder sb = new StringBuilder();
 			while (br.ready()) {
 				sb.append(br.readLine());
 			}
 			br.close();
-			String raw = sb.toString();
-			StringTokenizer st = new StringTokenizer(raw,",");
-			StackBasedList<Integer> sbl = new StackBasedTightList<Integer>();
-			long startTime = System.nanoTime();
-			while (st.hasMoreTokens()) {
-				sbl.push(new Integer(st.nextToken(",")));
-			}
-			System.out.println("operation completed in " + (System.nanoTime() - startTime) + " nanoseconds.");
-			System.out.println("size " + sbl.size());
+			integers = sb.toString();
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		// Growth
+		StringTokenizer st1 = new StringTokenizer(integers,",");
+		StackBasedList<Integer> sbl1 = new StackBasedGrowthList<Integer>();
+		long startTime1 = System.nanoTime();
+		while (st1.hasMoreTokens()) {
+			sbl1.push(new Integer(st1.nextToken(",")));
+		}
+		System.out.println("pushed " + sbl1.size() + " in " + (System.nanoTime() - startTime1) + " nanoseconds using Growth.");
+
+		// Tight
+		StringTokenizer st2 = new StringTokenizer(integers,",");
+		StackBasedList<Integer> sbl2 = new StackBasedTightList<Integer>();
+		long startTime2 = System.nanoTime();
+		while (st2.hasMoreTokens()) {
+			sbl2.push(new Integer(st2.nextToken(",")));
+		}
+		System.out.println("pushed " + sbl2.size() + " in " + (System.nanoTime() - startTime2) + " nanoseconds using Tight.");
 	}
 }
