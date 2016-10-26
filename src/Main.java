6654d865a105d02a0706c2095969b72d33518559
sardonyx072 <mithoffman11@gmail.com>
2016-10-26 11:10:16 -0500

    added method 2 for in-code generated random numbers, approaching limit for allowed number of array elements unless command line arguments on JVM given

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main (String[] args) {
+		// Method 1
+		/*
 		String integers = "";
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("res\\data.csv"));
@@ -39,5 +41,109 @@ public class Main {
 			sbl2.push(new Integer(st2.nextToken(",")));
 		}
 		System.out.println("pushed " + sbl2.size() + " in " + (System.nanoTime() - startTime2) + " nanoseconds using Tight.");
+		*/
+		
+		// Method 2
+		final int TRIALS = 32;
+		final int MAX_ARRAY_SIZE = 100000; // Integer.MAX_VALUE - 5;
+		final int MAX_RANDOM_NUMBER = Integer.MAX_VALUE;
+		int[] array;
+		String[][] results = new String[4][TRIALS+1];
+		int longestNumberLength = 8;
+		
+		for (int i = 0; i <= TRIALS; i++) {
+			StackBasedList<Integer> myArray;
+			long timeStart, timeStop, timeGrowth, timeTight;
+			array = new int[(MAX_ARRAY_SIZE / TRIALS)*i];
+			System.out.println("Test " + (i+1) + " with " + array.length + " data points:");
+			for (int j = 0; j < array.length; j++) {
+				array[j] = (int)(Math.random()*MAX_RANDOM_NUMBER);
+			}
+			System.out.println("\tfinished generating random numbers.");
+			
+			// Growth
+			myArray = new StackBasedGrowthList<Integer>();
+			System.out.println("\tmeasuring Growth style");
+			timeStart = System.nanoTime();
+			for (int k : array) {
+				myArray.push(k);
+			}
+			timeStop = System.nanoTime();
+			timeGrowth = timeStop - timeStart;
+			
+			// Tight
+			myArray = new StackBasedTightList<Integer>();
+			System.out.println("\tmeasuring Tight style");
+			timeStart = System.nanoTime();
+			for (int k : array) {
+				myArray.push(k);
+			}
+			timeStop = System.nanoTime();
+			timeTight = timeStop - timeStart;
+
+			results[0][i] = new Long(array.length).toString();
+			results[1][i] = new Long(timeGrowth).toString();
+			results[2][i] = new Long(timeTight-timeGrowth).toString();
+			results[3][i] = new Long(timeTight).toString();
+		}
+		
+		// Display Results
+		for (int i = 0; i < results.length; i++) {
+			for (int j = 0; j < results[0].length; j++) {
+				longestNumberLength = Math.max(longestNumberLength, results[i][j].length());
+			}
+		}
+		System.out.println();
+		
+		// 		Header
+		for (int i = 0; i < Math.floor((longestNumberLength - 8) / 2.0); i++) {
+			System.out.print(" ");
+		}
+		System.out.print("elements");
+		for (int i = 0; i < Math.ceil((longestNumberLength - 8) / 2.0); i++) {
+			System.out.print(" ");
+		}
+		System.out.print("||");
+		for (int i = 0; i < Math.floor((longestNumberLength - 6) / 2.0); i++) {
+			System.out.print(" ");
+		}
+		System.out.print("GROWTH");
+		for (int i = 0; i < Math.ceil((longestNumberLength - 6) / 2.0); i++) {
+			System.out.print(" ");
+		}
+		System.out.print("|");
+		for (int i = 0; i < Math.floor((longestNumberLength - 5) / 2.0); i++) {
+			System.out.print(" ");
+		}
+		System.out.print("DELTA");
+		for (int i = 0; i < Math.ceil((longestNumberLength - 5) / 2.0); i++) {
+			System.out.print(" ");
+		}
+		System.out.print("|");
+		for (int i = 0; i < Math.floor((longestNumberLength - 5) / 2.0); i++) {
+			System.out.print(" ");
+		}
+		System.out.print("TIGHT");
+		for (int i = 0; i < Math.ceil((longestNumberLength - 5) / 2.0); i++) {
+			System.out.print(" ");
+		}
+		System.out.println();
+		
+		// 		Data
+		for (int i = 0; i < results[0].length; i++) {
+			for (int j = 0; j < results.length; j++) {
+				for (int k = 0; k < (longestNumberLength - results[j][i].length()); k++) {
+					System.out.print(" ");
+				}
+				System.out.print(results[j][i]);
+				if (j == 0) {
+					System.out.print("||");
+				}
+				else if (j < results.length-1) {
+					System.out.print("|");
+				}
+			}
+			System.out.println();
+		}
 	}
 }
